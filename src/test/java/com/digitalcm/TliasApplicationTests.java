e0e45e99963141164fcc5ac78492153c2910805f
wangsong <384097785@qq.com>
2023-04-29 14:11:33 +0800

    Initial commit

---
+++ src/test/java/com/digitalcm/TliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
